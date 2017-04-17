ea1c3e86b1bc8e55f09ebfadb7de7767a6e10ad1
stephenwstrom <strom@acm.org>
2017-04-16 22:45:21 -0400

    First commit of geometric algebra project.

---
+++ GeometricAlgebra/GeometricAlgebra/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GeometricAlgebra
+//
+//  Created by STEPHEN STROM on 4/16/17.
+//  Copyright © 2017 STEPHEN STROM. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
